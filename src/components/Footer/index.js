f0bad6785c07ea4f38d1977c92eca51631a2ffac
Niklas Solomon <niklassolomon@gmail.com>
2022-11-08 20:43:44 -0800

    created footer style

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,8 @@
+import styles from './Footer.module.css';
+
 const Footer = () => {
     return (
-        <footer>
+        <footer className={styles.Footer}>
             <a href="#">Github</a>
             <a href="#">LinkedIn</a>
             <a href="#">Third Platform</a>
