5eb5ad377cde1bbfa45614be8543f7c99b19a50f
Niklas Solomon <niklassolomon@gmail.com>
2022-11-09 18:32:36 -0800

    added flex displays to containers

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div className={ styles.Page }>
             <h2>About Me</h2>
-            <p className={ styles.Content }>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi eum doloremque, quibusdam mollitia unde aspernatur et neque accusantium labore nostrum omnis similique nobis debitis perferendis odit dolorum atque ducimus inventore?</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi eum doloremque, quibusdam mollitia unde aspernatur et neque accusantium labore nostrum omnis similique nobis debitis perferendis odit dolorum atque ducimus inventore?</p>
         </div>
     )
 };
