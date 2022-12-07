f5bca5284bdb54894d2452c6fb1a342141892695
Niklas Solomon <niklassolomon@gmail.com>
2022-12-06 20:09:36 -0800

    updated link to resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -4,7 +4,7 @@ const Resume = () => {
     return (
         <div className={ styles.Page }>
             <h2>My Resume</h2>
-            <a className={styles.Button} href="https://docs.google.com/document/d/1BV9QzTRXfzkNMrSCaR65asqXmG-F9b7_im4TWI5IxHA/edit?usp=sharing" >Download</a>
+            <a className={styles.Button} href="https://docs.google.com/document/d/1K84Kp10lwlTdTak-hOCq4UKmhrBvBpDWDNcicwWeM9c/edit?usp=sharing" >Download</a>
             <div className={styles.Content}>
                 <h3>Proficiencies</h3>
                 <div>
